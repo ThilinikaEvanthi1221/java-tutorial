6753c28da19f9b241385d0b7e7c0e67a50e7ff91
Thilinika <evanthithilinika@gmail.com>
2024-10-08 14:30:15 +0530

    one

---
+++ Strings.java
@@ -0,0 +1,15 @@
+public class Strings {
+    public static void main(String[] args) {
+        String name1 = "Thilinika";
+        String name2 = "Evanthi";
+
+        String name3 = name1 + " " + name2;
+        System.out.println(name3);
+
+        // charAt
+        // to check what character in the given position
+
+        String name = "Aman";
+        System.out.println(name.charAt(0));
+    }
+}
---
+++ Types.java
@@ -0,0 +1,23 @@
+public class Types {
+    public static void main(String[] args) {
+        // byte - 1 [-128 to 127]
+        // short - 2
+        // int -4 (1,2,3)
+        // long - 8
+        // float - 4 (3.14)
+        // double - 8
+        // char - 2 (a,b,c,d)
+        // boolean - 1 (true/false)
+
+        byte age = 36;
+        int phone = 1234567890;
+        long phone2 = 12345678900L;
+        float pi = 3.14F;
+        char letter = '@';
+        boolean isAdult = false;
+
+        // non-primitive
+        String name = "Aman";
+        System.out.println(name.length());
+    }
+}
