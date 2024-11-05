3349aba500442cb2e837a3e6130ba005bccbe3df
Thilinika <evanthithilinika@gmail.com>
2024-11-05 12:31:40 +0530

    six

--- casting.java
+++ casting.java
@@ -1,12 +1,12 @@
 public class casting {
     public static void main(String[] args) {
         double price = 100.00;
-        double finalPrice = price + 18;
+        double finalPrice = price + 18; // implicit casting
 
         System.out.println(finalPrice);
 
         int p = 100;
-        int fp = p + (int) 18.0;
+        int fp = p + (int) 18.0; // explicit casting
         System.out.println(fp);
     }
 }
---
+++ operators.java
@@ -0,0 +1,16 @@
+public class operators {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+
+        System.out.println(sum);
+
+        int num = 1;
+        num++;
+        System.out.println(num);
+
+        // num++ change later
+        // ++num change before
+    }
+}
